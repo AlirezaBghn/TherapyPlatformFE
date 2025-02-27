c82b3ebc2e0e40cee082f867e3bbf094529f6ff7
Alireza Baghban <alireza.baghbann@gmail.com>
2025-02-27 18:41:23 +0100

    Fixes #28: Resolve text overflow issue by applying proper word wrapping

--- src/components/JournalCard.jsx
+++ src/components/JournalCard.jsx
@@ -1,15 +1,22 @@
 import { Link } from "react-router-dom";
 import PropTypes from "prop-types";
+import SkeletonLoader from "../components/loadings/SkeletonLoader";
 
-const JournalCard = ({ journal }) => {
+const JournalCard = ({ journal, loading }) => {
+  if (loading) {
+    return (
+      <SkeletonLoader count={1} skeletonColor="bg-gray-500" linesOnly={true} />
+    );
+  }
+
   return (
     <Link to={`/journal/${journal._id}`} className="block">
-      <div className=" relative bg-[#f3f4f6] dark:bg-gray-800 border border-gray-300 dark:border-gray-700 shadow-lg p-6 rounded-lg cursor-pointer transition duration-300 hover:border-gray-400 hover:shadow-xl group">
+      <div className="relative bg-[#f3f4f6] dark:bg-gray-800 border border-gray-300 dark:border-gray-700 shadow-lg p-6 rounded-lg cursor-pointer transition duration-300 hover:border-gray-400 hover:shadow-xl group">
         <div className="absolute inset-0 bg-gradient-to-r from-gray-900 to-transparent opacity-0 group-hover:opacity-20 transition duration-500"></div>
-        <h3 className="text-lg font-semibold text-gray-900 dark:text-white">
+        <h3 className="text-lg font-semibold text-gray-900 dark:text-white break-words">
           {journal.title}
         </h3>
-        <p className="text-sm mt-4 text-gray-900 dark:text-white">
+        <p className="text-sm mt-4 text-gray-900 dark:text-white break-words">
           {journal.content.substring(0, 100)}...
         </p>
         <div className="mt-6 flex space-x-4 relative z-10">
@@ -28,6 +35,7 @@ JournalCard.propTypes = {
     title: PropTypes.string.isRequired,
     content: PropTypes.string.isRequired,
   }).isRequired,
+  loading: PropTypes.bool,
 };
 
 export default JournalCard;
--- src/pages/UserPages/GetTipsAndAdvice.jsx
+++ src/pages/UserPages/GetTipsAndAdvice.jsx
@@ -1,6 +1,7 @@
-import React, { useContext, useState, useEffect } from "react";
+import { useContext, useState, useEffect } from "react";
 import { AdviceContext } from "../../context/AdviceContext";
 import DOMPurify from "dompurify"; // Import DOMPurify
+import RingLoader from "../../components/loadings/RingLoader";
 
 function GetTipsAndAdvice() {
   const { advice, fetchAdvice, loading } = useContext(AdviceContext);
@@ -15,22 +16,27 @@ function GetTipsAndAdvice() {
   }, [advice]);
 
   return (
-    <div className="flex items-center justify-center h-[calc(100vh-8rem)] px-4 bg-gray-100 dark:bg-gray-800">
+    <div className="flex items-center justify-center h-[calc(100vh-8rem)] px-4 bg-gray-100 dark:bg-gray-800 mt-12">
       <div className="flex flex-col items-center">
         {/* Heading positioned in the top right */}
-        <h1 className="text-3xl font-bold text-gray-800 dark:text-gray-200 mb-4">
+        <h1 className="text-3xl font-bold text-gray-800 dark:text-gray-200 mb-4 mt-4">
           Tips and Advice
         </h1>
 
         {/* Advice Card with max height and scroll */}
-        <div className="card card-side bg-base-100 dark:bg-gray-700 shadow-xl max-w-3xl w-full max-h-[60vh] overflow-auto p-4">
-          <div className="card-body p-6">
+        <div className="card card-side p-4 bg-gray-200  dark:bg-gray-600 shadow-xl max-w-3xl w-full max-h-[60vh] overflow-auto ">
+          <div className="card-body p-8 bg-gray-100 dark:bg-gray-500 shadow-xl max-w-3xl w-full max-h-[60vh] overflow-auto">
             {loading ? (
               <div>
                 <h2 className="text-gray-800 dark:text-gray-200">
                   Hold on tight, the AI is doing its magic!
                 </h2>
-                <div className="w-full h-24 bg-gray-300 dark:bg-gray-600 rounded-lg animate-pulse"></div>
+                <div
+                  className="flex justify-center items-center mt-5"
+                  style={{ transform: "scale(1.2)" }}
+                >
+                  <RingLoader />
+                </div>
               </div>
             ) : advice ? (
               <div
@@ -38,7 +44,7 @@ function GetTipsAndAdvice() {
                 dangerouslySetInnerHTML={{ __html: displayedAdvice }}
               ></div>
             ) : (
-              <p className="text-gray-500 dark:text-gray-400 text-lg">
+              <p className="text-gray-500 dark:text-gray-200 text-lg">
                 Click the button to get advice.
               </p>
             )}
