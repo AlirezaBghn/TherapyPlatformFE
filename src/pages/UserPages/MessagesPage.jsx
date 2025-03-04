893128d4f154b27601081de4c2c9df25c1232c1c
cvemir369 <cvemir369@gmail.com>
2025-03-04 11:50:05 +0100

    messages and chat in messages - styling

--- src/pages/UserPages/MessagesPage.jsx
+++ src/pages/UserPages/MessagesPage.jsx
@@ -3,6 +3,8 @@ import { axiosClient } from "../../services/api";
 import { useAuth } from "../../context/AuthContext";
 import Chat from "../../components/Chat";
 import SkeletonLoader from "../../components/loadings/SkeletonLoader";
+import { MessagesSquare, UserRound } from "lucide-react";
+import { Link } from "react-router-dom";
 
 const MessagesPage = () => {
   const { user } = useAuth();
@@ -56,33 +58,58 @@ const MessagesPage = () => {
 
   return (
     <div className="container mx-auto px-6 py-12 mt-24">
-      <h1 className="text-3xl font-bold mb-8">Your Therapists</h1>
+      <h1 className="text-3xl font-bold mb-8">Contacted Therapists</h1>
       <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
         {therapists.map((therapist) => (
           <div
             key={therapist._id}
-            className="p-6 bg-white dark:bg-gray-800 rounded-lg shadow-lg"
+            className="p-6 bg-white dark:bg-gray-800 rounded-lg shadow-lg flex gap-8"
           >
-            <h2 className="text-xl font-semibold">{therapist.name}</h2>
-            <button
-              onClick={() => setSelectedTherapist(therapist)}
-              className="mt-4 px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600 transition"
-            >
-              Chat
-            </button>
+            <div className="avatar">
+              <div className="w-24 rounded-full">
+                <img src={therapist.image} alt={`Photo of ${therapist.name}`} />
+              </div>
+            </div>
+            <div>
+              <h2 className="text-xl font-semibold">{therapist.name}</h2>
+              <a className="italic text-sm" href={`mailto:${therapist.email}`}>
+                {therapist.email}
+              </a>
+              <div className="flex justify-left items-center gap-3">
+                <Link
+                  to={`/therapist/${therapist._id}`}
+                  className="mt-3 px-6 py-2 text-lg font-semibold rounded bg-neutral-900 dark:bg-gray-200 text-white dark:text-gray-900 hover:bg-gray-700 dark:hover:bg-gray-300 transition duration-200"
+                >
+                  <UserRound />
+                </Link>
+                <button
+                  onClick={() => setSelectedTherapist(therapist)}
+                  className="mt-3 px-6 py-2 text-lg font-semibold rounded bg-neutral-900 dark:bg-gray-200 text-white dark:text-gray-900 hover:bg-gray-700 dark:hover:bg-gray-300 transition duration-200"
+                >
+                  <MessagesSquare size={24} />
+                </button>
+              </div>
+            </div>
           </div>
         ))}
       </div>
       {selectedTherapist && (
         <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50">
-          <div className="bg-white dark:bg-gray-700 rounded-lg shadow-xl w-full max-w-4xl mx-4">
-            <div className="flex justify-between items-center p-4 border-b border-gray-200 dark:border-gray-600">
-              <h2 className="text-xl font-semibold">
-                Chat with {selectedTherapist.name}
-              </h2>
+          <div className="bg-neutral-100 dark:bg-gray-700 rounded-lg shadow-xl w-full max-w-4xl mx-4">
+            <div className="flex justify-between items-center p-4 border-b border-neutral-400 dark:border-gray-600">
+              <div className="flex items-center gap-4">
+                <MessagesSquare
+                  className="text-neutral-800 dark:text-neutral-200"
+                  size={32}
+                />
+                <div className="text-xl font-semibold text-neutral-800 dark:text-neutral-200">
+                  Chat
+                </div>
+              </div>
+
               <button
                 onClick={() => setSelectedTherapist(null)}
-                className="text-gray-500 hover:text-gray-700 dark:text-gray-300 dark:hover:text-gray-400"
+                className="text-neutral-800 hover:text-gray-700 dark:text-gray-300 dark:hover:text-gray-400"
               >
                 Close
               </button>
